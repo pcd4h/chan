d2c278827ee0e10b2c17d6c98b508e74cd912c32
Dirk Haertel <dirk.haertel@gmail.com>
2015-06-19 16:49:49 +0200

    add site_config initializer

---
+++ config/initializers/site_config.rb
@@ -0,0 +1,4 @@
+VUM_USER_ACCOUNT = ENV["vum_user_account"]
+VUM_CONVERSATION_KEY = ENV["vum_conversation_key"]
+VUM_API_TOKEN = ENV["vum_api_token"]
+VUM_API_URL = ENV["vum_api_url_begin"] + VUM_CONVERSATION_KEY + "/messages.json"
