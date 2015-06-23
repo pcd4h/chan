a93e1e7b60fcabfd9e67b1907440b153f48eeefc
Dirk Haertel <dirk.haertel@gmail.com>
2015-06-23 13:17:23 +0200

    fix config

--- config/initializers/site_config.rb
+++ config/initializers/site_config.rb
@@ -1,4 +1,5 @@
 VUM_USER_ACCOUNT = ENV["vum_user_account"]
 VUM_CONVERSATION_KEY = ENV["vum_conversation_key"]
 VUM_API_TOKEN = ENV["vum_api_token"]
-VUM_API_URL = ENV["vum_api_url_begin"] + VUM_CONVERSATION_KEY + "/messages.json"
+VUM_API_URL_BEGIN = ENV["vum_api_url_begin"]
+VUM_API_URL_END = ENV["vum_api_url_end"]
